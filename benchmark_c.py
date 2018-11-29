cf7eaba05c7125d37da7f38051e71e17bdd9ec19
BrianLim167 <blim167@gmail.com>
2018-11-29 15:45:26 -0500

    done commissioning

--- benchmark_c.py
+++ benchmark_c.py
@@ -9,14 +9,16 @@ abort()
 
 ##try:
 def benchmark():
-    kill_disease()
-
-def kill_disease():
 ##    usr = input("Please type your NYU net ID: ")
     usr = "bl2667"
     a = NRC(usr)
     s = a.secret
-    a.login("b")
+    a.login("c")
+    
+    
+    #############################################################################
+    ##----------------------------DISEASE--------------------------------------##
+    #############################################################################
     diseases_cured = 0
     for organ in ["Heart_0","Stomach_0","BrainL_0"]:
         cured_diseases = 0
@@ -33,6 +35,7 @@ def kill_disease():
 ##            print('\n'*80)
             print("--------------------------------------------------------------------")
             print("Current location: "+status["player_position"])
+            print("Virus location: "+status["bot_location"])
             print("Finding organ: "+organ)
             print("Diseases cured: "+str(diseases_cured))
             heal = a.heal()
@@ -63,6 +66,7 @@ def kill_disease():
 ##        print('\n'*80)
         print("--------------------------------------------------------------------")
         print("Current location: "+status["player_position"])
+        print("Virus location: "+status["bot_location"])
         print("Checking the rest of this organ")
         heal = a.heal()
         print(heal.text)
@@ -75,8 +79,47 @@ def kill_disease():
                 input()
         a.tick()
 
-benchmark()
-abort()
+    
+    #############################################################################
+    ##-----------------------------VIRUS---------------------------------------##
+    #############################################################################
+    while True:
+        status = a.get_status().json()
+        if "Victory" in a.get_status().text:
+            print(a.get_status().text)
+            time.sleep(5)
+            return
+##        print('\n'*80)
+        print("--------------------------------------------------------------------")
+        print("Current location: "+status["player_position"])
+        print("Virus location: "+status["bot_location"])
+        virus_distance = ""
+        if status["bot_visible"]:
+            virus_distance = str(NRC.pathfinding(status["bot_location"])[status["player_position"]])
+        print("Virus distance: "+virus_distance)
+        print("Visibility: "+str(status["bot_visible"]))
+        print("Finding virus")
+        if status["number_of_options"] > 1:
+            a.start_holding()
+##            action = input("Choose action {},{},{},{}:".format(status["options"],"stop","go","abort"))
+##            if action == "stop":
+##                a.start_holding()
+##            elif action == "go":
+##                a.stop_holding()
+##            elif action == "abort":
+##                abort()
+##                return
+##            else:
+##                a.select_direction(action)
+        elif status["holding"]:
+            a.stop_holding()
+##        if status["number_of_options"] > 1:
+##            a.select_direction(random.randint(0, status["number_of_options"]-1))
+        a.tick()
+
+for i in range(2):
+    benchmark()
+    abort()
 
 ##except Exception as e:
 ##    print("########~~ERROR~~########")
